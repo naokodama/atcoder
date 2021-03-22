566dc0a77cf9eab0f5dfb3d9d78ec5837b81141f
naokodama <loaf.m.wind5420@gmail.com>
2021-03-23 00:08:30 +0900

    modify the bug of program c.

--- abc196/abc196_c/Main.java
+++ abc196/abc196_c/Main.java
@@ -9,17 +9,17 @@ class Main {
 
             Long n = scanner.nextLong();
             Long cnt = 0L;
-            for (long i = 0; i <= 1000000; i++) {
+            for (long i = 1; i <= 1000000; i++) {
                 String str = String.valueOf(i);
                 Long times = 1L;
                 for (int j = 0; j < str.length(); j++) {
                     times *= 10;
                 }
-                System.out.println(i * times);
-                if (n < i * times) {
+                System.out.println(i * times + i);
+                if (n < i * times + i) {
                     break;
                 }
-                cnt += 1;
+                cnt++;
             }
             System.out.println(cnt);
         } catch (InputMismatchException e) {
